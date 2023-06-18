73a96357dad2436eb614624f8ee69fb48006d466
prashant <prashantburde84@gmail.com>
2023-06-18 14:56:00 +0530

    remove folder

--- new_chnager/jsfile.js
+++
@@ -1,7 +0,0 @@
-let box = document.querySelector(".colorBox"),
-        input = document.querySelector("input");
-
-    input.addEventListener("input", () => {
-        box.style.backgroundColor = input.value;
-        box.style.borderRadius = input.value;
-    });
