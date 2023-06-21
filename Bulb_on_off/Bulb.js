48f57f86aa489c83e52d74760c0c673c1d386a65
prashant <prashantburde84@gmail.com>
2023-06-21 16:30:47 +0530

    Bulb

---
+++ Bulb_on_off/Bulb.js
@@ -0,0 +1,14 @@
+function bulbOn()
+{
+    let bulbobj = document.querySelector(".bulb_Off");
+
+    bulbobj.setAttribute("src","bulbOn.png");
+}
+
+
+function bulbOff()
+{
+    let bulbobj = document.querySelector(".bulb_Off");
+
+    bulbobj.setAttribute("src","bulbOff.png");
+}
