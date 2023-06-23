a878d41e6199363e4d1011ffc4c5bb9107b1257b
prashant <prashantburde84@gmail.com>
2023-06-23 17:52:06 +0530

    vowelcounter

---
+++ Vowel_counter/vowe.js
@@ -0,0 +1,23 @@
+var vowelCount = document.getElementById("vowelCount");
+
+
+function countVowel(str){
+    str = str.toLowerCase();
+
+ 
+
+   
+    let vowArr = ['a','e','i','o','u'],
+
+     counter = 0;
+
+
+    for (let letter of str) {
+        if(vowArr.includes(letter)) {
+            counter++;
+ 
+        }
+    }
+
+    vowelCount.innerHTML = counter;
+}
